94be4e9311ad2def743c194e82a2e7a4114f9199
qhxiv <eknight1412@gmail.com>
2024-07-21 20:58:05 +0700

    update

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -10,7 +10,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "sans-serif:pixelsize=13" };
+static const char *fonts[]          = { "sans-serif:pixelsize=13", "ShureTechMono Nerd Font Propo:pixelsize=13" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
